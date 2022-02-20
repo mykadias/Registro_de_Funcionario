1461e79a1bfc926701e83d0cf8d2cc9430c820a0
Mykaellen Dias <78829729+mykadias@users.noreply.github.com>
2022-02-20 01:24:05 -0300

    Update FuncionarioRepository.java

--- src/repositories/FuncionarioRepository.java
+++ src/repositories/FuncionarioRepository.java
@@ -8,19 +8,15 @@ import java.io.PrintWriter;
 public class FuncionarioRepository {
     public void gravarDados (Funcionario funcionario) throws Exception {
 
-        //CRIANDO ARQUIVO
         PrintWriter novoArquivo = new PrintWriter(new File("C:\\temp\\funcionario.txt"));
-
-        //ESCREVENDO DADOS NO ARQUIVO
+        
         novoArquivo.write("ID: " + funcionario.getIdFuncionario());
         novoArquivo.write("\nNome: "+ funcionario.getNome());
         novoArquivo.write("\nCPF: "+ funcionario.getCPF());
         novoArquivo.write("\nMatricula: "+funcionario.getMatricula());
         novoArquivo.write("\nSalário: R$ "+funcionario.getSalario());
 
-        //fechando e salvando o arquivo
         novoArquivo.flush();
         novoArquivo.close();
     }
 }
-
